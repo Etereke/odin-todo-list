183dbe2b3e43d2078733da6437e24a7e1bf1e582
Nagy-Zsugya Martin Albert <zsugya.martin@gmail.com>
2022-08-03 17:39:12 +0200

    Initialize project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # odin-todo-list
+
+Project made for [The Odin Project](https://www.theodinproject.com/lessons/node-path-javascript-todo-list).
+
+Used technologies: Html, CSS, JavaScript, Webpack
---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("It works");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHdCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vb2Rpbi10b2RvLWxpc3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJJdCB3b3Jrc1wiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
